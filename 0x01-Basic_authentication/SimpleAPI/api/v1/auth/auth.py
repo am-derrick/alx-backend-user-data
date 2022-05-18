7ba08909e075c2c91b380d57667075b09061dbdc
Derrick Ampire <ampire90@gmail.com>
2022-05-18 21:12:12 +0000

    Task files

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Module for Authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Authentication class
+    """
+    
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ checks if API require authentication
+        """
+        if path is None or not excluded_paths:
+            return True
+        if i in excluded_paths:
+            if i.endswith('*') and path.startswith(i[:-1]):
+                return False
+            elif i in {path, path + '/'}:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ checks for authorization header
+        """
+        if request is None or "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers.get('Authorization')
+
+    def current_user(self, request-None) -> TypeVar('User'):
+        """ public method for current user
+        """
+        return None
