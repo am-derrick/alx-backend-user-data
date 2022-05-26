8ba2c619e1339f3f3a7a05828823d8fa8359deef
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-05-26 23:56:20 +0300

    logout, profile, password reset and update implemented

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -64,5 +64,79 @@ def login() -> str:
     return response
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ DELETE /sessions
+    Destroys session by finding session_id (key in cookie)
+    Return:
+      - Redirects user to status route (GET /)
+    """
+    user_cookie = request.cookies.get("session_id", None)
+    user = AUTH.get_user_from_session_id(user_cookie)
+    if user_cookie is None or user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """ GET /profile
+    Return:
+        - Use session_id to find the user.
+        - 403 if session ID is invalid
+    """
+    user_cookie = request.cookies.get("session_id", None)
+    if user_cookie is None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(user_cookie)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """ POST /reset_password
+            - email
+        Return:
+            - Generate a Token
+            - 403 if email not registered
+    """
+    user_request = request.form
+    user_email = user_request.get('email')
+    is_registered = AUTH.create_session(user_email)
+
+    if not is_registered:
+        abort(403)
+
+    token = AUTH.get_reset_password_token(user_email)
+    message = {"email": user_email, "reset_token": token}
+    return jsonify(message)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """ PUT /reset_password
+            - email
+            - reset_token
+            - new_password
+        Return:
+            - Update the password
+            - 403 if token is invalid
+    """
+    user_email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except Exception:
+        abort(403)
+
+    message = {"email": user_email, "message": "Password updated"}
+    return jsonify(message), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
